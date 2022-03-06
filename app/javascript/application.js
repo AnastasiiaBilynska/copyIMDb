64093f6756773d2ad8c438efc6b812a4503d069b
AnastasiiaBilynska <nk015132@gmail.com>
2022-03-07 01:41:13 +0200

    fixed dropdown

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,15 +3,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 
-import "jquery/src/jquery"
 import 'bootstrap/js/dist/dropdown'
 import 'bootstrap/js/dist/collapse'
-
 
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-$(document).ready(function() { $(".dropdown-toggle").dropdown(); })
-import * as bootstrap from "bootstrap"
